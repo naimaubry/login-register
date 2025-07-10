1157500b6c7a5130e95c0c3a484f7fea3740f278
Naima Aubry-Romero <naima.aubry.usa@gmail.com>
2025-07-09 23:44:09 -0400

    enable insertion of data

--- registration.php
+++ registration.php
@@ -15,26 +15,39 @@
             $email = $_POST["email"];
             $password = $_POST["password"];
             $passwordRepeat = $_POST["repeat_password"];
+
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+            
             $errors = array();
-            if (empty($fullName) OR empty($email) OR empty($password) OR empty($password_Repeat)) {
-                array_push($errors, "All fields are required");
+            if (empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+                array_push($errors,"All fields are required");
             }
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 array_push($errors, "Email is not valid");
             }
             if (strlen($password)<8) {
-                array_push($errors, "Password must be at least 8 characters long");
+                array_push($errors,"Password must be at least 8 charactes long");
             }
-            if ($password !== $passwordRepeat) {
-                array_push($errors, "Password does not match");
+            if ($password!==$passwordRepeat) {
+                array_push($errors,"Password does not match");
             }
             if (count($errors)>0) {
-                foreach ($errors as $error) {
+                foreach ($errors as  $error) {
                     echo "<div class='alert alert-danger'>$error</div>";
                 }
-            }else {
-                //we will insert the data into the database
+            }else{
+            #insert data into database
+            $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
+            $stmt = mysqli_stmt_init($conn);
+            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+            if ($prepareStmt) {
+                mysqli_stmt_bind_param($stmt,"sss",$fullName, $email, $passwordHash);
+                mysqli_stmt_execute($stmt);
+                echo "<div class='alert alert-success'>You are registered successfully.</div>";
+            }else{
+                die("Something went wrong");
             }
+           }
         }
         ?>
         <form action="registration.php" method="post">
