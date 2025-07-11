9c401ef61f760601afe27dca8d2f75ed556719f9
Naima Aubry-Romero <naima.aubry.usa@gmail.com>
2025-07-11 18:35:31 -0400

    unique email + login file

--- registration.php
+++ registration.php
@@ -10,6 +10,10 @@
 <body>
     <div class="container"> 
         <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
         if (isset($_POST["submit"])) {
             $fullName = $_POST["fullname"];
             $email = $_POST["email"];
@@ -31,22 +35,32 @@
             if ($password!==$passwordRepeat) {
                 array_push($errors,"Password does not match");
             }
+
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $rowCount = mysqli_num_rows($result);
+            if ($rowCount>0) {
+                array_push($errors, "Email already exists!");
+            }
+
             if (count($errors)>0) {
                 foreach ($errors as  $error) {
                     echo "<div class='alert alert-danger'>$error</div>";
                 }
             }else{
-            #insert data into database
-            $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
-            $stmt = mysqli_stmt_init($conn);
-            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
-            if ($prepareStmt) {
-                mysqli_stmt_bind_param($stmt,"sss",$fullName, $email, $passwordHash);
-                mysqli_stmt_execute($stmt);
-                echo "<div class='alert alert-success'>You are registered successfully.</div>";
-            }else{
-                die("Something went wrong");
-            }
+                #insert data into database
+                $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
+                $stmt = mysqli_stmt_init($conn);
+                mysqli_error($conn);
+                $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
+                if ($prepareStmt) {
+                    mysqli_stmt_bind_param($stmt,"sss",$fullName, $email, $passwordHash);
+                    mysqli_stmt_execute($stmt);
+                    echo "<div class='alert alert-success'>You are registered successfully.</div>";
+                }else{
+                    die("Something went wrong");
+                }
            }
         }
         ?>
